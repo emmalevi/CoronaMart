78db119fa791847d407eb3973e43bfa5fc4554ae
Sagi Amar <sagitimamar@gmail.com>
2020-04-06 22:01:04 +0300

    created the index and show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,40 @@
 class ItemsController < ApplicationController
 
+before_action :set_item, only: [:show, :edit, :update, :destroy]
+
+def index
+    if params[:query].present?
+      @query = params[:query]
+      @items = Item.where("name iLike '%#{params[:query]}%'")
+    else
+      @items = Item.all
+    end
+  end
+
+def show
+end
+
+
+
+
+
+
+
+
+
+
+
+
+	private
+
+
+	def set_item
+	  @item= Item.find(params[:id])
+	end
+
+
+	def item_params
+	  params.require(:item).permit(:name, :description, :price)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'items#index'
+
   devise_for :users
-  root to: 'pages#home'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	get 'items',     to: 'items#index'
+
+
+  get 'items/:id', to: 'items#show', as: :item
+
 end
